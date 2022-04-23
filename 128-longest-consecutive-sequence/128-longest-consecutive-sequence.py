6cf151b76132c162ad1a9435c6cc2707fb732630
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-04-23 06:15:01 +0200

    Time: 180 ms (80.93%), Space: 20.2 MB (95.43%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,23 +4,28 @@ class Solution(object):
         if not nums:
             return 0
         
+        
         nums.sort()
-        currentSequence=1
-        maxSequence=0
+        
+        current=1
+        maximum=0
+        
         
         for i in range(1, len(nums)):
             
-            if nums[i]== nums[i-1]:
+            if nums[i]==nums[i-1]:
                 continue
-            if nums[i]== nums[i-1] + 1:
-                currentSequence+=1
+                
+            if nums[i]==nums[i-1] + 1:
+                current+=1
                 
             else :
-                maxSequence=max(currentSequence, maxSequence)
-                currentSequence=1
+                maximum=max(maximum, current)
+                current=1
                 
+        return max(maximum, current)
         
-        return max(maxSequence, currentSequence)
+        
                 
                 
             
