7e37d0ef03c9d7bb1992be7424efdae968b43a66
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-05-05 08:23:09 +0200

    Time: 59 ms (55.67%), Space: 13.8 MB (36.39%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        
+        hashtable={}
+        
+        res=[]
+        stack=[]
+        
+        
+        for i in range(len(nums2)):
+            
+            
+            while stack and nums2[i] > stack[-1]:
+                hashtable[stack[-1]]= nums2[i]
+                stack.pop()
+                
+            stack.append(nums2[i])
+            
+            
+        for i in range(len(stack)):
+            hashtable[stack[i]]= -1
+            
+            
+        for i in range(len(nums1)):
+            res.append(hashtable[nums1[i]])
+            
+        return res
+            
+            
+            
+            
+            
+        
+
+                
+                
+            
+                
+            
+            
+        
+        
