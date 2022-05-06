c4ffe38113f4dcd88a57dad28c9fb1d9ae3706eb
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-05-06 08:53:44 +0200

    Time: 22 ms (56.46%), Space: 13.4 MB (76.55%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        res=[]
+        
+        self.helper(root, res)
+        return res
+    
+    
+    def helper(self, root, res):
+        
+        if root :
+            self.helper(root.left, res)
+            self.helper(root.right, res)
+            res.append(root.val)
+       
