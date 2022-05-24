2a45d7f70759ea33e35f1cdd0281ca2a73897428
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-05-24 14:41:28 +0200

    Time: 76 ms (52.86%), Space: 25.2 MB (84.92%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,14 +8,13 @@
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         
-        if root is None or root ==p or root ==q:
+        if root is None or root==p or root==q:
             return root
         
         
-        left= self.lowestCommonAncestor(root.left, p, q)
         
-        
-        right= self.lowestCommonAncestor(root.right, p, q)
+        left= self.lowestCommonAncestor(root.left, p , q)
+        right=self.lowestCommonAncestor(root.right, p, q)
         
         
         if left and right:
@@ -25,5 +24,4 @@ class Solution(object):
         return left or right
         
         
-        
-        
+       
