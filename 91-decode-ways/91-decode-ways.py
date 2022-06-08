223a80243cb28a96f115d81402cc5e3df7063e16
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-06-08 09:21:06 +0530

    Time: 42 ms (12.12%), Space: 13.5 MB (55.55%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def numDecodings(self, s):
+        
         dp={len(s):1}
         
         
@@ -11,10 +12,12 @@ class Solution(object):
             else:
                 dp[i]=dp[i+1]
                 
-            if (i+1 <len(s) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456" )):
+                
+            if (i+1 <len(s) and(s[i]=="1" or s[i]=="2" and s[i+1] in "0123456")):
                 dp[i]+=dp[i+2]
                 
         return dp[0]
+       
                 
        
         
