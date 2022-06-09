a44dd84a674567f0f6aa999f626f5cd4481da0ed
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-06-09 20:48:50 +0530

    Time: 828 ms (97.03%), Space: 24.6 MB (96.04%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        
+        
+        one_buy= two_buy = float('inf')
+        one_profit=two_profit= 0
+        
+        
+        for price in prices:
+            
+            one_buy=min(one_buy, price)
+            one_profit=max(one_profit, price- one_buy)
+            two_buy=min(two_buy, price-one_profit )
+            two_profit=max(two_profit, price-two_buy)
+            
+            
+        return two_profit
+        
+            
+        
+       
+        
